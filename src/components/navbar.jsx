f367210c6f01d5ef6284fca75828b04b398b6744
ibnegufran <ibnegufran75074@gmail.com>
2024-07-24 11:29:51 +0530

    changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -47,12 +47,12 @@ const Navbar = () => {
   
   return (
 <>
-<div className={`z-50 w-[100vw] py-2 px-6 justify-content-between bg-white border-bottom-1 border-300  flex align-items-center md:justify-content-around  ${scrollToggle == true ?" md:fixed md:top-0 md:left-0" :"md:relative"}`} >
+<div className={`z-50 w-[100vw] py-2 px-6 justify-content-between bg-white border-bottom-1 border-300  flex align-items-center md:justify-content-around  ${scrollToggle == true ?"fixed top-0 left-0" :"relative"}`} >
     <div className="">
     <h1 className={`uppercase text-[2.5rem] font-bold  text-primary-color  `}>al-kahf</h1>
     <span className='text-2xl italic text-[#E6B392]'>online market</span>
     </div>
-    <div className={`absolute  text-white  ${toggle == false ?"top-[-100vw]":"top-[8vh]"}  text-3xl left-0 gap-6 p-4 flex items-start bg-[rgba(0,0,0,.3)] w-screen  flex-col md:relative md:flex md:top-0 md:items-center md:flex-row  md:gap-8 md:text-2xl capitalize transition-all duration-1000 md:bg-white md:p-0 md:text-black md:w-auto `} >
+    <div className={`absolute  text-white  ${toggle == false || scrollToggle == true ?"top-[-100vw]":"top-[99%]"}  text-3xl left-0 gap-6 p-4 flex items-start bg-[rgba(0,0,0,.3)] w-screen  flex-col md:relative md:flex md:top-0 md:items-center md:flex-row  md:gap-8 md:text-2xl capitalize transition-all duration-1000 md:bg-white md:p-0 md:text-black md:w-auto `} >
         <Link to="/" className="no-underline hover:text-black md:hover:text-gary300 md:text-black">home</Link>
         <Link to="/about"  className="no-underline hover:text-black md:hover:text-gary300 md:text-black">about</Link>
         <Link to="/shop"  className="no-underline hover:text-black md:hover:text-gary300 md:text-black">shop</Link>
